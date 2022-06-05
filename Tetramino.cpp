e6bed09e292828dcdfcbbc366438cfd59be9c8f6
DaniGarMata <99959458+DaniGarMata@users.noreply.github.com>
2022-06-05 13:19:49 +0200

    Small fixes
    Intro from other branch added, small error fix.

---
+++ Tetramino.cpp
@@ -0,0 +1,52 @@
+#include "Tetramino.h"
+
+#include "Application.h"
+#include "ModuleCollisions.h"
+#include "ModuleParticles.h"
+#include "ModuleAudio.h"
+#include "ModuleRender.h"
+
+Tetramino::Tetramino(int x, int y) : position(x, y)
+{
+	spawnPos = position;
+}
+
+Tetramino::~Tetramino()
+{
+	if (collider != nullptr)
+		collider->pendingToDelete = true;
+}
+
+const Collider* Tetramino::GetCollider() const
+{
+	return collider;
+}
+
+void Tetramino::Update()
+{
+	if (currentAnim != nullptr)
+		currentAnim->Update();
+
+	if (collider != nullptr)
+		collider->SetPos(position.x, position.y);
+}
+
+void Tetramino::Draw()
+{
+	if (currentAnim != nullptr)
+		App->render->Blit(texture, position.x, position.y, &(currentAnim->GetCurrentFrame()));
+}
+
+void Tetramino::OnCollision(Collider* collider)
+{
+
+
+	SetToDelete();
+}
+
+void Tetramino::SetToDelete()
+{
+	pendingToDelete = true;
+	if (collider != nullptr)
+		collider->pendingToDelete = true;
+}
